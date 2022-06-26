8247909e22171088cac268cff7d848125747b2a2
Sreedev10 <sreedevskurup10@gmail.com>
2022-06-26 09:25:25 -0700

    completed

---
+++ employee.js
@@ -0,0 +1,52 @@
+loginBtnEvent=()=>{
+    EmployeeName=(document.getElementById("EmployeeName").value)
+    password=(document.getElementById("password").value)
+    
+    console.log(EmployeeName)
+    alert(EmployeeName)
+
+    console.log(password)
+    alert(password)
+
+}
+
+registerBtnEvent=()=>{
+     
+    employeename=(document.getElementById("employeename").value)
+    address=(document.getElementById("address").value)
+    age=(document.getElementById("age").value)
+    emailid=(document.getElementById("email id").value)
+    mobilenumber=(document.getElementById("mobile number").value)
+    YearsofExperience=(document.getElementById("YearsofExperience").value)
+    password=(document.getElementById("password").value)
+    confirmpassword=(document.getElementById("confirm password").value)
+
+    console.log(employeename)
+    alert(employeename)
+
+    console.log(address)
+    alert(address)
+
+    console.log(age)
+    alert(age)
+
+    console.log(emailid)
+    alert(emailid)
+
+    console.log(mobilenumber)
+    alert(mobilenumber)
+
+    console.log(YearsofExperience)
+    alert(YearsofExperience)
+
+    console.log(password)
+    alert(password)
+
+    console.log(confirmpassword)
+    alert(confirmpassword)
+
+   
+
+}
+
+
